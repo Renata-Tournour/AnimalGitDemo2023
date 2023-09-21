a7faa7818476d4e27d8eda5466101ea2f7bde8c9
Renata <rt.henriques@campus.fct.unl.pt>
2023-09-21 20:07:06 +0100

    s version of our main class.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@
     public class Main {
         public static void main(String[] args) {
 
-            System.out.println("Super Animal git demo");
+            System.out.println("Super AAAnimal git demo!");
         }
     }
 
